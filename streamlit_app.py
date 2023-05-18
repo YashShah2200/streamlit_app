89854d2d307fefc345ca9f93b503a97cea5109f7
YashShah2200 <87566416+YashShah2200@users.noreply.github.com>
2023-05-18 15:02:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ streamlit.text('🐔 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥑Kale, Spinach & Rocket Smoothie')
 streamlit.text('🍞Hard-Boiled Free-Range Egg')
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇') 
 
 
 import pandas
